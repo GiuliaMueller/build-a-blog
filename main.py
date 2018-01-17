84ab8998b55205b801f07305367fed98fac5536b
Giulia Mueller <giulia_mueller@yahoo.com>
2018-01-16 19:02:25 -0500

    updated files

--- main.py
+++ main.py
@@ -1,49 +1,61 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect,render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build_a_blog:123@localhost:8889/build_a_blog'
+app.config["DEBUG"] = True
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://build_a_blog:123@localhost:8889/build_a_blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.Text)
+    
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
 
-class Task(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120))
-    completed = db.Column(db.Boolean)
-
-    def __init__(self, name):
-        self.name = name
-        self.completed = False
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route("/", methods = ["POST", "GET"])
 def index():
+    blogs = Blog.query.order_by(Blog.id.desc()).all()
+    return render_template("blog.html", blogs = blogs)
 
-    if request.method == 'POST':
-        task_name = request.form['task']
-        new_task = Task(task_name)
-        db.session.add(new_task)
+@app.route("/blog", methods = ["POST", "GET"])
+def blog():
+    blogid = request.args.get("id")
+    if blogid:
+        blog = Blog.query.filter_by(id=blogid).first()
+        return render_template("singleblog.html", blog=blog)
+    else:
+        blogs = Blog.query.order_by(Blog.id.desc()).all()
+        return render_template("blog.html", blogs = blogs)
+
+@app.route("/newpost", methods = ["POST", "GET"])
+def newpost():
+    return render_template("newpost.html")
+
+@app.route("/todos", methods=["POST", "GET"])
+def todos():
+    title = request.form["title"]
+    title_error = ""
+    body = request.form["body"]
+    body_error = ""
+
+    if title == "":
+        title_error = "Why don't you write something?"
+        return render_template("newpost.html", title=title, title_error = title_error)
+    if body == "":
+        body_error = "Why don't you write something?"
+        return render_template("newpost.html", body=body, body_error = body_error)
+    elif request.method == "POST":
+        blogpost = Blog(title, body)
+        db.session.add(blogpost)
+ #       db.session.flush()
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed=False).all()
-    completed_tasks = Task.query.filter_by(completed=True).all()
-    return render_template('todos.html',title="Build a Blog", 
-        tasks=tasks, completed_tasks=completed_tasks)
-
-
-@app.route('/delete-task', methods=['POST'])
-def delete_task():
-
-    task_id = int(request.form['task-id'])
-    task = Task.query.get(task_id)
-    task.completed = True
-    db.session.add(task)
-    db.session.commit()
-
-    return redirect('/')
+    idnum = blogpost.id
+    return redirect("/blog?id={0}".format(idnum))
 
 
 if __name__ == '__main__':
